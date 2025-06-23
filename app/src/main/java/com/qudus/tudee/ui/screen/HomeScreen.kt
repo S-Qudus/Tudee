352adc3cb1794144b12d591cb9cd12e26e5a3036
Ismail Elkhalili <ismail.elkalili@gmail.com>
2025-06-23 16:14:07 +0300

    -add BottomNavBar, icon and create  BottomNavItem data class -implemented BottomNavBar composable with equal-weighted clickable items -created HomeScreen composable with internal NavController

---
+++ app/src/main/java/com/qudus/tudee/ui/screen/HomeScreen.kt
@@ -0,0 +1,51 @@
+package com.qudus.tudee.ui.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.qudus.tudee.R
+import com.qudus.tudee.designSystem.model.BottomNavItem
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import com.qudus.tudee.designSystem.component.BottomNavBar
+
+@Composable
+fun HomeScreen() {
+
+    val navController = rememberNavController()
+    val items = listOf(
+        BottomNavItem(
+            route = "home",
+            iconFill = painterResource(R.drawable.home_icon_filled),
+            iconStroke = painterResource(R.drawable.home_icon_stroke)
+        ),
+        BottomNavItem(
+            route = "note",
+            iconFill = painterResource(R.drawable.note_icon_filled),
+            iconStroke = painterResource(R.drawable.note_icon_stroke)
+        ),
+        BottomNavItem(
+            route = "more",
+            iconFill = painterResource(R.drawable.more_icon_filled),
+            iconStroke = painterResource(R.drawable.more_icon_stroke)
+        )
+    )
+    val currentBackStackEntry = navController.currentBackStackEntryAsState()
+    val currentRoute = currentBackStackEntry.value
+        ?.destination?.route ?: "home"
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") { Text("Home Screen") }
+        composable("note") { Text("Note Screen") }
+        composable("more") { Text("More Screen") }
+    }
+
+    BottomNavBar(
+    navController = navController,
+    items = items,
+    selectedRoute = currentRoute
+    )
+
+}
