6acc3312fa71e851474c0efc5be776d244116015
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-06-23 13:19:47 +0300

    add CategoryBadgeItem composable with text and check badge components

---
+++ app/src/main/java/com/qudus/tudee/ui/composable/CategoryBadgeItem.kt
@@ -0,0 +1,101 @@
+package com.qudus.tudee.ui.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.qudus.tudee.R
+import com.qudus.tudee.designSystem.theme.Theme
+
+@Composable
+fun CategoryBadgeItem(
+    title: String,
+    iconPainter: Painter,
+    iconTint: Color,
+    modifier: Modifier = Modifier,
+    isClickable: Boolean = false,
+    onItemClick: () -> Unit = {},
+    badge: @Composable BoxScope.() -> Unit,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Box {
+            Icon(
+                modifier = Modifier
+                    .background(color = Theme.color.surfaceHigh, shape = CircleShape)
+                    .clip(CircleShape)
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = ripple(color = Theme.color.primaryVariant),
+                        enabled = isClickable,
+                        onClick = onItemClick
+                    )
+                    .padding(24.dp),
+                painter = iconPainter,
+                contentDescription = title,
+                tint = iconTint
+            )
+
+            badge()
+        }
+
+        Text(
+            text = title,
+            style = Theme.textStyle.label.small,
+            color = Theme.color.body
+        )
+    }
+}
+
+
+@Preview(showSystemUi = false, showBackground = true)
+@Composable
+private fun CategoryTextBadgeItemPreview() {
+    CategoryBadgeItem(
+        title = "Education",
+        iconPainter = painterResource(R.drawable.icon_book_open),
+        iconTint = Theme.color.purpleAccent,
+    ) {
+        TudeeTextBadge(
+            modifier = Modifier.align(Alignment.TopEnd),
+            badgeNumber = "16",
+            contentColor = Theme.color.hint,
+            containerColor = Theme.color.surfaceLow
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+private fun CategoryCheckBadgeItemPreview() {
+    CategoryBadgeItem(
+        title = "Education",
+        iconPainter = painterResource(R.drawable.icon_book_open),
+        iconTint = Theme.color.purpleAccent,
+        isClickable = true,
+        onItemClick = {}
+    ) {
+        TudeeCheckBadge(modifier = Modifier.align(Alignment.TopEnd), visible = true)
+    }
+}
---
+++ app/src/main/java/com/qudus/tudee/ui/composable/TudeeBadge.kt
@@ -0,0 +1,77 @@
+package com.qudus.tudee.ui.composable
+
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.qudus.tudee.R
+import com.qudus.tudee.designSystem.theme.Theme
+
+@Composable
+fun TudeeTextBadge(
+    badgeNumber: String,
+    modifier: Modifier = Modifier,
+    contentColor: Color = Color(0x611F1F1F),
+    containerColor: Color = Color(0xFFF0F0F0)
+) {
+    Text(
+        modifier = modifier
+            .widthIn(min = 32.dp)
+            .background(containerColor, shape = RoundedCornerShape(percent = 50))
+            .padding(vertical = 2.dp, horizontal = 4.dp),
+        text = badgeNumber,
+        color = contentColor,
+        textAlign = TextAlign.Center,
+        maxLines = 1
+    )
+}
+
+@Preview
+@Composable
+private fun TudeeTextBadgePreview() {
+    TudeeTextBadge(
+        badgeNumber = "16",
+        contentColor = Theme.color.hint,
+        containerColor = Theme.color.surfaceLow
+    )
+}
+
+@Composable
+fun TudeeCheckBadge(visible: Boolean, modifier: Modifier = Modifier) {
+    AnimatedVisibility(
+        modifier = modifier,
+        visible = visible,
+        enter = scaleIn(),
+        exit = scaleOut()
+    ) {
+        Icon(
+            modifier = Modifier
+                .background(color = Theme.color.greenAccent, shape = CircleShape)
+                .padding(4.dp),
+            painter = painterResource(R.drawable.icon_tick_double),
+            tint = Theme.color.onPrimary,
+            contentDescription = "$visible"
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TudeeCheckedBadge() {
+    TudeeCheckBadge(visible = true)
+}
+
