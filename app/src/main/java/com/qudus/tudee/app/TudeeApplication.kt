e9db6a098b43c528dd78c28b8345ad972a39a8a0
NadeenRR <nadeenradwan0@gmail.com>
2025-06-25 12:55:06 +0300

    - remove GlobalContext

--- app/src/main/java/com/qudus/tudee/app/TudeeApplication.kt
+++ app/src/main/java/com/qudus/tudee/app/TudeeApplication.kt
@@ -3,12 +3,12 @@ package com.qudus.tudee.app
 import android.app.Application
 import com.qudus.tudee.app.di.appModule
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.GlobalContext
+import org.koin.core.context.startKoin
 
 class TudeeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        GlobalContext.startKoin {
+        startKoin {
             androidContext(this@TudeeApplication)
             modules(appModule)
         }
