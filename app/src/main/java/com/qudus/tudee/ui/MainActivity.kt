c93f28d26ddb54716ae15f06002faaaaf70de52e
Ismail Elkhalili <ismail.elkalili@gmail.com>
2025-06-25 12:44:03 +0300

    adding libs.kotlinx.datetime to dependencies

--- app/src/main/java/com/qudus/tudee/ui/MainActivity.kt
+++ app/src/main/java/com/qudus/tudee/ui/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.qudus.tudee
+package com.qudus.tudee.ui
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -11,8 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.qudus.tudee.designSystem.theme.Theme
-import com.qudus.tudee.designSystem.theme.TudeeTheme
+import com.qudus.tudee.ui.designSystem.theme.Theme
+import com.qudus.tudee.ui.designSystem.theme.TudeeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
