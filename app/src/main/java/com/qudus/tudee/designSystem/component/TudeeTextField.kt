16f33dcdab9b1eeeae09796d3d03f83e1eedd72a
Farah315 <fara734isgreat@gmail.com>
2025-06-25 20:18:04 +0300

    customizable TudeeTextField

--- app/src/main/java/com/qudus/tudee/designSystem/component/TudeeTextField.kt
+++ app/src/main/java/com/qudus/tudee/designSystem/component/TudeeTextField.kt
@@ -18,24 +18,28 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.qudus.tudee.designSystem.theme.Theme
 
 @Composable
 fun TudeeTextField(
     modifier: Modifier = Modifier,
-    icon: Painter,
-    hintText: String,
     value: String,
     onTextChange: (String) -> Unit,
+    hintText: String,
+    icon: Painter? = null,
+    singleLine: Boolean = true,
+    maxLength: Int = if (singleLine) 100 else 500,
+    height: Dp? = if (singleLine) 56.dp else 168.dp,
     imeAction: ImeAction = ImeAction.Default,
-    onImeOptions: () -> Unit = {}
+    onImeAction: () -> Unit = {}
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val isFocused by interactionSource.collectIsFocusedAsState()
@@ -46,7 +50,7 @@ fun TudeeTextField(
     )
 
     val iconTint by animateColorAsState(
-        targetValue = if (isFocused || value.isNotEmpty()) Theme.color.body else Theme.color.hint,
+        targetValue = if ((isFocused || value.isNotEmpty())) Theme.color.body else Theme.color.hint,
         label = "icon tint"
     )
 
@@ -54,39 +58,22 @@ fun TudeeTextField(
         modifier = modifier
             .fillMaxWidth()
             .border(1.dp, color = borderColor, shape = RoundedCornerShape(16.dp))
+            .padding(if (icon == null) 12.dp else 0.dp)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(56.dp)
-                .padding(horizontal = 12.dp, vertical = 8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = icon,
-                contentDescription = "user icon",
-                tint = iconTint,
-                modifier = Modifier.padding(end = 12.dp)
-            )
-            Box(
-                modifier = Modifier
-                    .width(1.dp)
-                    .height(30.dp)
-                    .background(Theme.color.stroke)
-            )
+        if (icon == null) {
             BasicTextField(
                 value = value,
-                onValueChange = { newValue ->
-                    if (newValue.length <= 100) {
-                        onTextChange(newValue)
-                    }
+                onValueChange = {
+                    if (it.length <= maxLength) onTextChange(it)
                 },
                 modifier = Modifier
-                    .padding(start = 12.dp)
-                    .weight(1f),
+                    .fillMaxWidth()
+                    .height(height ?: 168.dp),
+                singleLine = singleLine,
                 textStyle = Theme.textStyle.body.medium.copy(color = Theme.color.body),
                 interactionSource = interactionSource,
-                singleLine = true,
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = imeAction),
+                keyboardActions = KeyboardActions(onAny = { onImeAction() }),
                 decorationBox = { innerTextField ->
                     if (value.isEmpty()) {
                         Text(
@@ -96,14 +83,53 @@ fun TudeeTextField(
                         )
                     }
                     innerTextField()
-                },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = imeAction
-                ),
-                keyboardActions = KeyboardActions(
-                    onAny = { onImeOptions() }
-                ),
+                }
             )
+        } else {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(height ?: 56.dp)
+                    .padding(horizontal = 12.dp, vertical = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = icon,
+                    contentDescription = null,
+                    tint = iconTint,
+                    modifier = Modifier.padding(end = 12.dp)
+                )
+                Box(
+                    modifier = Modifier
+                        .width(1.dp)
+                        .height(30.dp)
+                        .background(Theme.color.stroke)
+                )
+                BasicTextField(
+                    value = value,
+                    onValueChange = {
+                        if (it.length <= maxLength) onTextChange(it)
+                    },
+                    modifier = Modifier
+                        .padding(start = 12.dp)
+                        .weight(1f),
+                    textStyle = Theme.textStyle.body.medium.copy(color = Theme.color.body),
+                    interactionSource = interactionSource,
+                    singleLine = singleLine,
+                    keyboardOptions = KeyboardOptions.Default.copy(imeAction = imeAction),
+                    keyboardActions = KeyboardActions(onAny = { onImeAction() }),
+                    decorationBox = { innerTextField ->
+                        if (value.isEmpty()) {
+                            Text(
+                                text = hintText,
+                                style = Theme.textStyle.label.medium,
+                                color = Theme.color.hint
+                            )
+                        }
+                        innerTextField()
+                    }
+                )
+            }
         }
     }
 }
