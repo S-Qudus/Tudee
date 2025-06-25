ca99e0c72ad70df9dd1cda2102633b16b1dba898
mohammed-akkad <momoo362514@gmail.com>
2025-06-25 11:22:21 +0300

    refactor tudee buttons

--- app/src/main/java/com/qudus/tudee/ui/composable/TudeeButton.kt
+++ app/src/main/java/com/qudus/tudee/ui/composable/TudeeButton.kt
@@ -3,7 +3,6 @@ package com.qudus.tudee.ui.composable
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -13,10 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.qudus.tudee.R
 import com.qudus.tudee.designSystem.theme.Theme
 import com.qudus.tudee.designSystem.theme.TudeeTheme
 
@@ -84,7 +81,7 @@ private fun SubmitButtonPrev() {
         TudeeButton(
             onClick = {},
             isLoading = true,
-            title ="submit",
+            title = "submit",
             isEnabled = true,
             hasBorder = false,
             isNegativeButton = true,
--- app/src/main/java/com/qudus/tudee/ui/composable/TudeeTextButton.kt
+++ app/src/main/java/com/qudus/tudee/ui/composable/TudeeTextButton.kt
@@ -2,17 +2,14 @@ package com.qudus.tudee.ui.composable
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.qudus.tudee.R
 import com.qudus.tudee.designSystem.theme.Theme
 import com.qudus.tudee.designSystem.theme.TudeeTheme
 
