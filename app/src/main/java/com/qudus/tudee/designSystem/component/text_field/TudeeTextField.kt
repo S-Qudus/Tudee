654fe02b96e95d0d071f79578444f6d98506f915
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-06-26 09:37:55 +0300

    refactor TudeeTextField

--- app/src/main/java/com/qudus/tudee/designSystem/component/text_field/TudeeTextField.kt
+++ app/src/main/java/com/qudus/tudee/designSystem/component/text_field/TudeeTextField.kt
@@ -1,4 +1,5 @@
-package com.qudus.tudee.designSystem.component
+package com.qudus.tudee.designSystem.component.text_field
+
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -27,7 +28,6 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.qudus.tudee.R
 import com.qudus.tudee.designSystem.theme.Theme
@@ -36,59 +36,53 @@ import com.qudus.tudee.designSystem.theme.Theme
 fun TudeeTextField(
     modifier: Modifier = Modifier,
     value: String,
-    tudeeTextFieldType: TudeeTextFieldType,
-    onTextChange: (String) -> Unit,
-    hintText: String,
-    imeAction: ImeAction = ImeAction.Default,
-    onImeAction: () -> Unit = {},
-    maxLines: Int
+    type: TudeeTextFieldType,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    keyboardAction: ImeAction = ImeAction.Default,
+    onKeyboardAction: () -> Unit = {},
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val isFocused by interactionSource.collectIsFocusedAsState()
-
     val borderColor by animateColorAsState(
         targetValue = if (isFocused) Theme.color.primary else Theme.color.stroke,
         label = "border color"
     )
-
     val iconTint by animateColorAsState(
         targetValue = if ((isFocused || value.isNotEmpty())) Theme.color.body else Theme.color.hint,
         label = "icon tint"
     )
+    when (type) {
+        is TudeeTextFieldType.Paragraph -> TextField(
+            value = value,
+            hintText = placeholder,
+            maxLength = type.maxLength,
+            onTextChange = onValueChange,
+            interactionSource = interactionSource,
+            imeAction = keyboardAction,
+            onImeAction = onKeyboardAction,
+            maxLines = type.maxLines,
+            modifier = modifier
+                .border(1.dp, color = borderColor, shape = RoundedCornerShape(16.dp))
+                .padding(horizontal = 12.dp)
+                .height(type.height)
+        )
 
-    Box(
-        modifier = modifier
-            .fillMaxWidth()
-            .border(1.dp, color = borderColor, shape = RoundedCornerShape(16.dp))
-            .padding(if (tudeeTextFieldType is TudeeTextFieldType.WithIcon) 12.dp else 0.dp)
-
-    ) {
-        when (tudeeTextFieldType) {
-            is TudeeTextFieldType.Paragraph -> TextField(
-                value = value,
-                hintText = hintText,
-                maxLength = tudeeTextFieldType.maxLength,
-                onTextChange = onTextChange,
-                interactionSource = interactionSource,
-                imeAction = imeAction,
-                onImeAction = onImeAction,
-                maxLines = maxLines,
-
-                )
-            is TudeeTextFieldType.WithIcon -> TextWithIcon(
-                value = value,
-                hintText = hintText,
-                icon = tudeeTextFieldType.icon,
-                iconTint = iconTint,
-                maxLength = tudeeTextFieldType.maxLength,
-                onTextChange = onTextChange,
-                interactionSource = interactionSource,
-                imeAction = imeAction,
-                onImeAction = onImeAction,
-                maxLines = maxLines,
-
-                )
-        }
+        is TudeeTextFieldType.WithIcon -> TextWithIcon(
+            value = value,
+            hintText = placeholder,
+            icon = type.icon,
+            iconTint = iconTint,
+            maxLength = type.maxLength,
+            onTextChange = onValueChange,
+            interactionSource = interactionSource,
+            imeAction = keyboardAction,
+            onImeAction = onKeyboardAction,
+            maxLines = type.maxLines,
+            modifier = modifier
+                .border(1.dp, color = borderColor, shape = RoundedCornerShape(16.dp))
+                .height(type.height)
+        )
     }
 }
 
@@ -103,40 +97,40 @@ private fun TextWithIcon(
     interactionSource: MutableInteractionSource,
     imeAction: ImeAction,
     onImeAction: () -> Unit,
+    maxLines: Int,
     modifier: Modifier = Modifier,
-    maxLines: Int
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(horizontal = 12.dp, vertical = 8.dp),
+            .padding(start = 12.dp)
+            .padding(vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
             painter = icon,
             contentDescription = null,
             tint = iconTint,
-            modifier = Modifier.padding(end = 12.dp)
+            modifier = Modifier
         )
         Box(
             modifier = Modifier
-                .width(maxLines.dp)
+                .padding(start = 12.dp, end = 12.dp)
+                .width(1.dp)
                 .height(30.dp)
                 .background(Theme.color.stroke)
         )
         TextField(
             value = value,
             modifier = Modifier
-                .weight(1F)
-                .padding(start = 12.dp),
-            onTextChange = {
-                if (it.length <= maxLength) onTextChange(it)
-            },
+                .weight(1F),
+            onTextChange = onTextChange,
             interactionSource = interactionSource,
             imeAction = imeAction,
             onImeAction = onImeAction,
             hintText = hintText,
-            maxLines = maxLines
+            maxLines = maxLines,
+            maxLength = maxLength
         )
     }
 }
@@ -144,14 +138,14 @@ private fun TextWithIcon(
 @Composable
 private fun TextField(
     value: String,
-    modifier: Modifier = Modifier,
-    maxLength: Int = 1,
     onTextChange: (String) -> Unit,
     interactionSource: MutableInteractionSource,
     imeAction: ImeAction,
     onImeAction: () -> Unit,
     hintText: String,
-    maxLines: Int
+    modifier: Modifier = Modifier,
+    maxLines: Int = 1,
+    maxLength: Int = 100,
 ) {
     BasicTextField(
         value = value,
@@ -178,21 +172,6 @@ private fun TextField(
     )
 }
 
-sealed class TudeeTextFieldType {
-    data class Paragraph(
-        val maxLength: Int = 500,
-        val maxLines: Int = 10,
-        val height: Dp = 168.dp
-    ) : TudeeTextFieldType()
-
-    data class WithIcon(
-        val icon: Painter,
-        val maxLines: Int = 1,
-        val maxLength: Int = 100,
-        val height: Dp = 56.dp
-    ) : TudeeTextFieldType()
-}
-
 @Preview(showBackground = true)
 @Composable
 fun PreviewTudeeTextFieldWithIcon() {
@@ -200,14 +179,13 @@ fun PreviewTudeeTextFieldWithIcon() {
 
     TudeeTextField(
         value = text.value,
-        onTextChange = { text.value = it },
-        hintText = "hi there its farah",
-        tudeeTextFieldType = TudeeTextFieldType.WithIcon(
+        onValueChange = { text.value = it },
+        placeholder = "hi there its farah",
+        type = TudeeTextFieldType.WithIcon(
             icon = painterResource(id = R.drawable.icon_user)
         ),
-        imeAction = ImeAction.Done,
-        onImeAction = {},
-        maxLines = 1,
+        keyboardAction = ImeAction.Done,
+        onKeyboardAction = {},
     )
 }
 
@@ -218,17 +196,16 @@ fun PreviewTudeeTextFieldParagraph() {
 
     TudeeTextField(
         value = text.value,
-        onTextChange = { text.value = it },
-        hintText = "As a squad, our task is to develop a personal task management app for Android. The app should store data locally using the Room database and implement Jetpack Navigation 2 for seamless user flow management.\n" +
+        onValueChange = { text.value = it },
+        placeholder = "As a squad, our task is to develop a personal task management app for Android. The app should store data locally using the Room database and implement Jetpack Navigation 2 for seamless user flow management.\n" +
                 "\n" +
                 "Use Koin for dependency injection, ensuring that higher-level modules do not depend on lower-level ones. Adhere strictly to the SOLID principles learned in Season 1.\n" +
                 "\n" +
                 "Maintain a simple architecture—only include components that are necessary and justified, anything without reason will affect your score in negative way!.\n" +
                 "\n" +
-                "Each screen is expected to have its own ViewModel. The ViewModel should depend on an abstraction called TasksServices, which will provide domain-level models called entities. The implementation of TasksServices should utilize DAOs from Room to retrieve and map data into domain entities.\n"
-        ,tudeeTextFieldType = TudeeTextFieldType.Paragraph(),
-        imeAction = ImeAction.Default,
-        onImeAction = {},
-        maxLines = 10
+                "Each screen is expected to have its own ViewModel. The ViewModel should depend on an abstraction called TasksServices, which will provide domain-level models called entities. The implementation of TasksServices should utilize DAOs from Room to retrieve and map data into domain entities.\n",
+        type = TudeeTextFieldType.Paragraph(),
+        keyboardAction = ImeAction.Default,
+        onKeyboardAction = {},
     )
 }
