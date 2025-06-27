c841aee5d98a83e938025729021bf7a0ca1ac2e4
Farah315 <fara734isgreat@gmail.com>
2025-06-28 00:55:29 +0300

    change image path to string

--- app/src/main/java/com/qudus/tudee/ui/state/CategoryUiState.kt
+++ app/src/main/java/com/qudus/tudee/ui/state/CategoryUiState.kt
@@ -1,11 +1,8 @@
 package com.qudus.tudee.ui.state
 
-import androidx.annotation.DrawableRes
-
 data class CategoryUiState (
-    val id: String="0",
-    val title:String="",
-     @DrawableRes
-     val image: Int? = null,
-     val isPredefined: Boolean= false
+    val id: String = "0",
+    val title: String = "",
+    val image: String="",
+    val isPredefined: Boolean = false
 )
