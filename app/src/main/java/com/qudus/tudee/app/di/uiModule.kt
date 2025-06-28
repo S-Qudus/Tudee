974f348b03a8bf250ddbbed9dff05869924b8c31
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-06-28 22:17:45 +0300

    Refactor Koin modules for services, data, and UI
    This commit reorganizes and enhances the Koin dependency injection setup:
    
    - **servicesModule**:
        - Registers `CategoryServiceImpl`, `TaskServiceImpl`, and `InputValidator` as singletons.
    - **dataModule**:
        - Configures Room database with a specific name, callback, and DAOs (`TaskDao`, `CategoryDao`).
        - Introduces `DatabaseConstants` for managing database-related string qualifiers and values.
    - **uiModule**:
        - Adds `AddTaskViewModel` to the dependency graph.

--- app/src/main/java/com/qudus/tudee/app/di/uiModule.kt
+++ app/src/main/java/com/qudus/tudee/app/di/uiModule.kt
@@ -1,7 +1,9 @@
 package com.qudus.tudee.app.di
 
+import com.qudus.tudee.ui.screen.addTask.AddTaskViewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val uiModule = module{
-
+    viewModelOf(::AddTaskViewModel)
 }
