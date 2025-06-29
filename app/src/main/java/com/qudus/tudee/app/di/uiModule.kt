aefa1ea49e9aacb6266c0e918432a89d038ea3f0
Farah315 <fara734isgreat@gmail.com>
2025-06-29 12:00:08 +0300

    add view model

--- app/src/main/java/com/qudus/tudee/app/di/uiModule.kt
+++ app/src/main/java/com/qudus/tudee/app/di/uiModule.kt
@@ -1,7 +1,12 @@
 package com.qudus.tudee.app.di
 
+import com.qudus.tudee.ui.screen.categorysheet.AddCategoryViewModel
+import com.qudus.tudee.ui.screen.categorysheet.EditCategoryViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val uiModule = module{
+    viewModel { AddCategoryViewModel(get()) }
+    viewModel { EditCategoryViewModel(get()) }
 
 }
