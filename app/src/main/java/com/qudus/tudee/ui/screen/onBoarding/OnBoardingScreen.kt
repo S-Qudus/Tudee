3ea813929d026103f5e572095791cfbb42879710
mohammed-akkad <momoo362514@gmail.com>
2025-06-29 12:05:50 +0300

    refactor on boarding screen

--- app/src/main/java/com/qudus/tudee/ui/screen/onBoarding/OnBoardingScreen.kt
+++ app/src/main/java/com/qudus/tudee/ui/screen/onBoarding/OnBoardingScreen.kt
@@ -1,11 +1,11 @@
 package com.qudus.tudee.ui.screen.onBoarding
 
-import android.util.Log
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,9 +27,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -86,141 +92,233 @@ private fun OnBoardingContent(
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(Theme.color.surface)
             .background(Theme.color.overlay),
+        contentAlignment = Alignment.BottomCenter
     ) {
-        val ellipseBackground =
-            if (onBoardingUiState.isDarkTheme) R.drawable.ellipse_background_dark else R.drawable.ellipse_background
-        Image(
-            painter = painterResource(ellipseBackground),
-            modifier = Modifier.offset(x = (30).dp),
-            contentDescription = "ellipse_background"
+        EllipseContent(onBoardingUiState)
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+
+            PagerContent(
+                onBoardingUiState = onBoardingUiState,
+                pagerState = pagerState,
+                onBoardingInteractionListener = onBoardingInteractionListener
+            )
+            IndicatorPager(
+                onBoardingUiState = onBoardingUiState,
+                pagerState = pagerState
+            )
+
+        }
+
+    }
+    SkipText(
+        onBoardingUiState = onBoardingUiState,
+        pagerState = pagerState,
+        onBoardingInteractionListener = onBoardingInteractionListener
+    )
+}
+
+@Composable
+private fun EllipseContent(onBoardingUiState: OnBoardingUiState) {
+    val ellipseBackgroundStartColor =
+        if (onBoardingUiState.isDarkTheme) Color(0x7F05202E) else Color(0x7FEFF9FE)
+
+    val ellipseBackgroundEndColor =
+        if (onBoardingUiState.isDarkTheme) Color(0x0005202E) else Color(0x00EFF9FE)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .offset(y = 90.dp)
+                .rotate(135f)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(ellipseBackgroundStartColor, ellipseBackgroundEndColor),
+                        start = Offset(x = 0f, y = 0f),
+                        end = Offset(x = 75f, y = 355f),
+
+                        )
+                ),
         )
 
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .offset(x = 100.dp, y = 120.dp)
+                .rotate(140f)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(ellipseBackgroundStartColor, ellipseBackgroundEndColor),
+                        start = Offset(x = 0f, y = 0f),
+                        end = Offset(x = 75f, y = 355f),
+                    )
+                ),
+        )
 
-        Column(modifier = Modifier.fillMaxSize()) {
-            HorizontalPager(
-                state = pagerState,
-            ) { pagerIndex ->
-                Column {
-                    Log.d("uiState 123123", "$onBoardingUiState")
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .offset(x = 200.dp, y = 320.dp)
+                .rotate(140f)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(ellipseBackgroundStartColor, ellipseBackgroundEndColor),
+                        start = Offset(x = 0f, y = 0f),
+                        end = Offset(x = 75f, y = 355f),
+                    )
+                ),
+        )
+    }
+}
 
-                    Spacer(modifier = Modifier.padding(86.dp))
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(350.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Image(
-                            modifier = Modifier
-                                .size(width = 296.dp, height = 250.dp)
-                                .offset(y = 30.dp),
-                            painter = painterResource(onBoardingUiState.onBoardingItemUiState[pagerIndex].imageRes),
-                            contentDescription = ""
-                        )
-                    }
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(220.dp)
-                            .padding(Theme.dimension.medium),
-                        contentAlignment = Alignment.Center
-                    ) {
-
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(192.dp)
-                                .background(
-                                    color = Theme.color.onPrimaryCard,
-                                    shape = RoundedCornerShape(32.dp)
-                                )
-                                .border(
-                                    width = 1.dp,
-                                    color = Theme.color.onPrimaryStroke,
-                                    shape = RoundedCornerShape(32.dp)
-                                )
-                        ) {
-                            Column {
-                                Text(
-                                    text = stringResource(onBoardingUiState.onBoardingItemUiState[pagerIndex].title),
-                                    style = Theme.textStyle.title.medium,
-                                    color = Theme.color.title,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(horizontal = 16.dp)
-                                        .padding(top = 24.dp),
-                                    textAlign = TextAlign.Center
-                                )
-                                Text(
-                                    text = stringResource(onBoardingUiState.onBoardingItemUiState[pagerIndex].description),
-                                    style = Theme.textStyle.body.medium,
-                                    color = Theme.color.body,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(horizontal = 16.dp)
-                                        .padding(top = 32.dp),
-                                    textAlign = TextAlign.Center
-                                )
-                            }
-
-                        }
-
-                        TudeeFloatingActionButton(
-                            onClickIconButton = { onBoardingInteractionListener.onClickNextButton() },
-                            isEnabled = true,
-                            isLoading = false,
-                            painter = painterResource(R.drawable.icon_arrow_right),
-                            modifier = Modifier.offset(y = 90.dp)
-                        )
-
-                    }
-                }
-
-            }
-
-            Row(
+@Composable
+private fun PagerContent(
+    onBoardingUiState: OnBoardingUiState,
+    pagerState: PagerState,
+    onBoardingInteractionListener: OnBoardingInteractionListener
+) {
+    HorizontalPager(
+        state = pagerState,
+    ) { pagerIndex ->
+        Column {
+            Spacer(modifier = Modifier.padding(86.dp))
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 24.dp, top = 32.dp),
-                horizontalArrangement = Arrangement.Center
+                    .height(250.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(width = 296.dp, height = 250.dp)
+                        .offset(y = 30.dp),
+                    painter = painterResource(onBoardingUiState.onBoardingItemUiState[pagerIndex].imageRes),
+                    contentDescription = ""
+                )
+            }
+            Spacer(modifier = Modifier.padding(top = 32.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(220.dp)
+                    .padding(Theme.dimension.medium),
+                contentAlignment = Alignment.Center
             ) {
 
-                repeat(onBoardingUiState.onBoardingItemUiState.size) { index ->
-                    val isSelected = pagerState.currentPage == index
-                    val dividerColor by animateColorAsState(if (isSelected) Theme.color.primary else Theme.color.primaryVariant)
-
-                    Box(
-                        modifier = Modifier
-                            .size(height = 5.dp, width = 100.dp)
-                            .background(
-                                color = dividerColor,
-                                shape = CircleShape
-                            )
-                    )
-
-                    if (index < 2) {
-                        Spacer(modifier = Modifier.padding(horizontal = 10.dp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(192.dp)
+                        .background(
+                            color = Theme.color.onPrimaryCard,
+                            shape = RoundedCornerShape(32.dp)
+                        )
+                        .border(
+                            width = 1.dp,
+                            color = Theme.color.onPrimaryStroke,
+                            shape = RoundedCornerShape(32.dp)
+                        )
+                ) {
+                    Column {
+                        Text(
+                            text = stringResource(onBoardingUiState.onBoardingItemUiState[pagerIndex].title),
+                            style = Theme.textStyle.title.medium,
+                            color = Theme.color.title,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp)
+                                .padding(top = 24.dp),
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = stringResource(onBoardingUiState.onBoardingItemUiState[pagerIndex].description),
+                            style = Theme.textStyle.body.medium,
+                            color = Theme.color.body,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp)
+                                .padding(top = 32.dp),
+                            textAlign = TextAlign.Center
+                        )
                     }
 
                 }
-            }
-        }
-        Box(modifier = Modifier.padding(start = 16.dp, top = 64.dp)) {
-            if (pagerState.currentPage != onBoardingUiState.onBoardingItemUiState.lastIndex) {
-                Text(
-                    text = stringResource(R.string.skip),
-                    style = Theme.textStyle.label.large,
-                    color = Theme.color.primary,
-                    modifier = Modifier
-                        .clickable {
-                            onBoardingInteractionListener.onClickSkipText()
-                        }
+
+                TudeeFloatingActionButton(
+                    onClickIconButton = { onBoardingInteractionListener.onClickNextButton() },
+                    isEnabled = true,
+                    isLoading = false,
+                    painter = painterResource(R.drawable.icon_arrow_right),
+                    modifier = Modifier.offset(y = 90.dp)
                 )
 
             }
         }
 
+    }
+}
+
+@Composable
+private fun SkipText(
+    onBoardingUiState: OnBoardingUiState,
+    pagerState: PagerState,
+    onBoardingInteractionListener: OnBoardingInteractionListener
+) {
+    Box(modifier = Modifier.padding(start = 16.dp, top = 64.dp)) {
+        if (pagerState.currentPage != onBoardingUiState.onBoardingItemUiState.lastIndex) {
+            Text(
+                text = stringResource(R.string.skip),
+                style = Theme.textStyle.label.large,
+                color = Theme.color.primary,
+                modifier = Modifier
+                    .clickable(
+                        onClick = { onBoardingInteractionListener.onClickSkipText() },
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() }
+                    )
+            )
+
+        }
+    }
+
+}
+
+@Composable
+private fun IndicatorPager(onBoardingUiState: OnBoardingUiState, pagerState: PagerState) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 24.dp, top = 40.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+
+        repeat(onBoardingUiState.onBoardingItemUiState.size) { index ->
+            val isSelected = pagerState.currentPage == index
+            val dividerColor by animateColorAsState(if (isSelected) Theme.color.primary else Theme.color.primaryVariant)
+
+            Box(
+                modifier = Modifier
+                    .size(height = 5.dp, width = 100.dp)
+                    .background(
+                        color = dividerColor,
+                        shape = CircleShape
+                    )
+            )
+
+            if (index < 2) {
+                Spacer(modifier = Modifier.padding(horizontal = 10.dp))
+            }
+
+        }
     }
 }
 
