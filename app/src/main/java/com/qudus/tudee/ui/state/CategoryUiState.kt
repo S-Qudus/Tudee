a01d41b074cffa466c0031e126a41544aa63acb4
Farah315 <fara734isgreat@gmail.com>
2025-06-29 11:57:54 +0300

    edit CategoryUiState

--- app/src/main/java/com/qudus/tudee/ui/state/CategoryUiState.kt
+++ app/src/main/java/com/qudus/tudee/ui/state/CategoryUiState.kt
@@ -1,8 +1,10 @@
 package com.qudus.tudee.ui.state
 
 data class CategoryUiState (
-    val id: String = "0",
+    val id: Long = 0L,
     val title: String = "",
     val image: String="",
-    val isPredefined: Boolean = false
+    val isPredefined: Boolean = false,
+    val isEditMode: Boolean = false,
+    val isTitleValid: Boolean = true
 )
