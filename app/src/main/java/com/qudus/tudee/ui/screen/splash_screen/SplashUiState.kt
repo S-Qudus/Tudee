084b93bbe7b2b2965fa1fb5c77b7d2281ad91841
Hazem Alkateb <hazem22-1@hotmail.com>
2025-06-30 17:13:57 +0300

    delete files not required for splash screen api

--- app/src/main/java/com/qudus/tudee/ui/screen/splash_screen/SplashUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.qudus.tudee.ui.screen.splash_screen
-
-data class SplashUiState(
-    val isCompleteOnBoarding: Boolean = false,
-    val isLoading: Boolean = true,
-    val isDarkTheme: Boolean = false,
-)
