5160c0b1eefbe34b2300dea94d0e82843b6590b1
Farah315 <fara734isgreat@gmail.com>
2025-06-30 14:49:42 +0300

    use viewModelOf

--- app/src/main/java/com/qudus/tudee/app/di/uiModule.kt
+++ app/src/main/java/com/qudus/tudee/app/di/uiModule.kt
@@ -1,17 +1,16 @@
 package com.qudus.tudee.app.di
 
+import com.qudus.tudee.ui.screen.addCategorySheet.AddCategoryViewModel
 import com.qudus.tudee.ui.screen.addTask.AddTaskViewModel
+import com.qudus.tudee.ui.screen.editCategoryScreen.EditCategoryViewModel
 import com.qudus.tudee.ui.screen.onBoarding.OnBoardingViewModel
 import org.koin.core.module.dsl.viewModelOf
-import com.qudus.tudee.ui.screen.categorysheet.AddCategoryViewModel
-import com.qudus.tudee.ui.screen.categorysheet.EditCategoryViewModel
-import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val uiModule = module{
     viewModelOf(::AddTaskViewModel)
     viewModelOf(::OnBoardingViewModel)
-    viewModel { AddCategoryViewModel(get()) }
-    viewModel { EditCategoryViewModel(get()) }
+    viewModelOf(:: AddCategoryViewModel)
+    viewModelOf(::EditCategoryViewModel)
 
 }
