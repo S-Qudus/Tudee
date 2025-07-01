36798cbdb1f4e585d6e13e1b61b3560152defda8
mohammed-akkad <momoo362514@gmail.com>
2025-07-01 12:22:35 +0300

    refactor wrap service call

--- app/src/main/java/com/qudus/tudee/data/util/WrapCall.kt
+++ app/src/main/java/com/qudus/tudee/data/util/WrapCall.kt
@@ -10,61 +10,34 @@ import com.qudus.tudee.domain.exception.TaskNotFoundException
 import com.qudus.tudee.domain.exception.TaskUpsertFailedException
 import com.qudus.tudee.domain.exception.TudeeExecption
 
+private fun mapRoomException(isCategory: Boolean, exception: Throwable): TudeeExecption {
+    return when (exception) {
+        is SQLiteConstraintException -> if (isCategory) CategoryUpsertFailedException() else TaskUpsertFailedException()
+        is IllegalStateException -> if (isCategory) CategoryNotFoundException() else TaskNotFoundException()
+        is SQLiteException -> if (isCategory) CategoryDeletionFailedException() else TaskDeletionFailedException()
+        else -> TudeeExecption()
+    }
+}
+
 suspend fun <T> wrapServiceSuspendCall(
     isCategory: Boolean = false,
     call: suspend () -> T
 ): T {
     return try {
         call()
-    } catch (_: SQLiteConstraintException) {
-        if (isCategory) {
-            throw CategoryUpsertFailedException()
-        } else {
-            throw TaskUpsertFailedException()
-        }
-    } catch (_: IllegalStateException) {
-        if (isCategory) {
-            throw CategoryNotFoundException()
-        } else {
-            throw TaskNotFoundException()
-        }
-    } catch (_: SQLiteException) {
-        if (isCategory) {
-            throw CategoryDeletionFailedException()
-        } else {
-            throw TaskDeletionFailedException()
-        }
-    } catch (_: Exception) {
-        throw TudeeExecption()
+    } catch (exception: Exception) {
+        throw mapRoomException(isCategory, exception)
     }
 }
 
 fun <T> wrapServiceCall(
     isCategory: Boolean = false,
-    call:() -> T
+    call: () -> T
 ): T {
     return try {
         call()
-    } catch (_: SQLiteConstraintException) {
-        if (isCategory) {
-            throw CategoryUpsertFailedException()
-        } else {
-            throw TaskUpsertFailedException()
-        }
-    } catch (_: IllegalStateException) {
-        if (isCategory) {
-            throw CategoryNotFoundException()
-        } else {
-            throw TaskNotFoundException()
-        }
-    } catch (_: SQLiteException) {
-        if (isCategory) {
-            throw CategoryDeletionFailedException()
-        } else {
-            throw TaskDeletionFailedException()
-        }
-    } catch (_: Exception) {
-        throw TudeeExecption()
+    } catch (exception: Exception) {
+        throw mapRoomException(isCategory, exception)
     }
 }
 
