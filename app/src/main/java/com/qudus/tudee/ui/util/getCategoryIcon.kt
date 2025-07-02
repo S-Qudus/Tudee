db94b242153dcec59d34e78cf390a381202ab8d2
Mohammed-qmr <mma.mqamar@gmail.com>
2025-07-02 13:47:06 +0300

    fix(tasks): resolve critical issues in task screen on Task TablItem

--- app/src/main/java/com/qudus/tudee/ui/util/getCategoryIcon.kt
+++ app/src/main/java/com/qudus/tudee/ui/util/getCategoryIcon.kt
@@ -1,5 +1,5 @@
 package com.qudus.tudee.ui.state
-package com.qudus.tudee.ui.util
+
 
 
 
@@ -9,8 +9,6 @@ import com.qudus.tudee.domain.entity.DefaultCategoryType
 fun getCategoryIcon(categoryType: DefaultCategoryType?): Int {
     return when (categoryType) {
         DefaultCategoryType.EDUCATION -> R.drawable.icon_category_book_open
-        DefaultCategoryType.SHOPPING -> R.drawable.icon_calendar
-        DefaultCategoryType.WORK -> R.drawable.icon_chef
         else -> R.drawable.icon_airplane
     }
 }
