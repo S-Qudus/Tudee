6bdcba99b63e4e52e9ed61ae212ccbfafd0cf596
Farah315 <fara734isgreat@gmail.com>
2025-07-02 12:40:56 +0300

    apply changes

--- app/src/main/java/com/qudus/tudee/app/di/uiModule.kt
+++ app/src/main/java/com/qudus/tudee/app/di/uiModule.kt
@@ -1,10 +1,11 @@
 package com.qudus.tudee.app.di
 
-import com.qudus.tudee.ui.screen.task_details.TaskDetailsViewModel
 import com.qudus.tudee.ui.screen.addTask.AddTaskViewModel
 import com.qudus.tudee.ui.screen.editCategoryScreen.EditCategoryViewModel
+import com.qudus.tudee.ui.screen.addCategoryScreen.AddCategoryViewModel
 import com.qudus.tudee.ui.screen.editTask.EditTaskViewModel
 import com.qudus.tudee.ui.screen.onBoarding.OnBoardingViewModel
+import com.qudus.tudee.ui.screen.task_details.TaskDetailsViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
